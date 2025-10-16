b9c9e99aedd8705aa554244fe853cc1db3c5fd55
Sai Teja Gajula <gajulasaiteja08@gmail.com>
2025-10-16 17:25:16 -0500

    Added promblem 4:Article_views.py

---
+++ 01_Data_Filtering/Article_views.py
@@ -0,0 +1,30 @@
+"""
+Views
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key (column with unique values) for this table, the table may have duplicate rows.
+Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+Note that equal author_id and viewer_id indicate the same person.
+ 
+
+Write a solution to find all the authors that viewed at least one of their own articles.
+
+Return the result table sorted by id in ascending order.
+"""
+
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+   views=(views.loc[views["author_id"]==views["viewer_id"],["viewer_id"]]
+              .drop_duplicates()
+              .rename(columns={"viewer_id":"id"})
+              .sort_values("id")
+              .reset_index(drop=True))
+   return views
